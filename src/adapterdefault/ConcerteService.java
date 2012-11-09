8d2e130c46e9f2c7e26a65b9269955f793c6b966
libiun <libiun@gmail.com>
2012-11-09 11:14:03 +0800

    默认适配器模式

---
+++ src/adapterdefault/ConcerteService.java
@@ -0,0 +1,9 @@
+package adapterdefault;
+
+public class ConcerteService extends ServiceAdapter {
+
+    @Override
+    public void service1() {
+        System.out.println("执行业务方法");
+    }
+}
---
+++ src/adapterdefault/ServiceAdapter.java
@@ -0,0 +1,16 @@
+package adapterdefault;
+
+public class ServiceAdapter implements AbstractService {
+
+    @Override
+    public void service1() {
+    }
+
+    @Override
+    public void service2() {
+    }
+
+    @Override
+    public void service3() {
+    }
+}
